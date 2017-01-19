574f9578452cb28c6c0b4c6d0a51e1d4bd42e3fd
Shuxuan Nie <shuxuan.nie@gmail.com>
2017-01-18 20:32:11 -0800

    update soa and osb binaries installation file

---
+++ soa/build.sh
@@ -0,0 +1,10 @@
+# BUILD THE IMAGE (replace all environment variables)
+NOCACHE=true
+BUILD_START=$(date '+%s')
+docker build --force-rm=$NOCACHE --no-cache=$NOCACHE -t oracle/soa:12.2.1.2 -f Dockerfile_soa . || {
+  echo "There was an error building the image."
+  exit 1
+}
+
+BUILD_END=$(date '+%s')
+BUILD_ELAPSED=`expr $BUILD_END - $BUILD_START`
