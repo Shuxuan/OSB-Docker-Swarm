269a2edb43f50ed59e569ca0c9cd722f6cd1f6a0
Shuxuan Nie <shuxuan.nie@gmail.com>
2017-02-26 09:38:02 -0800

    update osb rcu

---
+++ osb-domain/container-scripts/rcuOSB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# This script will wait until Admin Server is available.
+# There is no timeout!
+#
+echo Run rcu for SOA Infrastucture
+export ORACLE_HOME=/u01/oracle/weblogic
+export RCU_OSB_RSP=rcuOSB.rsp
+export RCU_OSB_PWD=rcuOSBPasswords.txt
+export RCU_INSTALL_HOME=/u01/oracle
+
+$ORACLE_HOME/oracle_common/bin/rcu -silent -responseFile $RCU_INSTALL_HOME/$RCU_OSB_RSP -f < $RCU_INSTALL_HOME/$RCU_OSB_PWD
