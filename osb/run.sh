8676234fd87f0ab03af8422884f6350313357eb6
Shuxuan Nie <shuxuan.nie@gmail.com>
2017-02-26 22:46:35 -0800

    update run

--- osb/run.sh
+++ osb/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name wlsadmin --hostname wlsadmin -p 7001:7001 oracle/osb:12.2.1.2
+docker run -d --name wlsadmin --add-host osbdb:172.17.0.2 --hostname wlsadmin -p 7001:7001 oracle/osb:12.2.1.2
