b9b31f7cbcb0f7f7b1474faa52e057997e9828f1
Shuxuan Nie <shuxuan.nie@gmail.com>
2017-02-26 22:58:32 -0800

    osb domain

--- osb/container-scripts/rcuOSB.sh
+++ osb/container-scripts/rcuOSB.sh
@@ -10,6 +10,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export ORACLE_HOME=/u01/oracle/weblogic
 export RCU_OSB_RSP=rcuResponseFile.properties
+export RCU_OSB_RSP_DEL=rcuResponseFile_del.properties
 export RCU_OSB_PWD=rcuOSBPasswords.txt
 
+$ORACLE_HOME/oracle_common/bin/rcu -silent -responseFile $DIR/$RCU_OSB_RSP_DEL -f < $DIR/$RCU_OSB_PWD
 $ORACLE_HOME/oracle_common/bin/rcu -silent -responseFile $DIR/$RCU_OSB_RSP -f < $DIR/$RCU_OSB_PWD
