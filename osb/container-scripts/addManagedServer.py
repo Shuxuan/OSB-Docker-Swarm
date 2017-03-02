39f3c02a72c986bd1f72f0e73e2edec13c6ba687
axreynol <axreynol@198.105.254.23>
2017-03-02 00:31:38 -0700

    First Cut Machine & Server Creation

--- osb/container-scripts/addManagedServer.py
+++ osb/container-scripts/addManagedServer.py
@@ -3,14 +3,16 @@ import os
 mwHome = os.getenv('MW_HOME', '/u01/oracle/weblogic')
 localTemplate = os.getenv('OSB_TEMPLATE', mwHome + '/user_projects/osbManagedTemplate.jar')
 domainPath = os.getenv('DOMAIN_DIR', mwHome + '/user_projects/osbDomain')
-adminHostname = os.getenv('ADMIN_HOSTNAME', 'localhost')
+adminHostname = os.getenv('ADMIN_HOSTNAME', 'wlsadmin')
 adminPort = os.getenv('ADMIN_PORT', '7001')
 adminUsername = os.getenv('ADMIN_USERNAME', 'weblogic')
-adminPassword = os.getenv('ADMIN_PASSWORD', 'welcome1')
-managedServername = os/getenv('MS_NAME', 'osb1')
+adminPassword = os.getenv('ADMIN_PASSWORD', 'Welcome1')
+managedServername = os.getenv('MS_NAME', 'osb_server1')
+machineName = os.getenv('MACHINE_NAME', 'osb1Machine')
+hostname = os.getenv(("HOSTNAME', 'localhost')
  
 #Substitute the administrator user name and password values below as needed
-connect(adminUsername, adminPassword, adminstHostname+':'+adminPort)
+connect(adminUsername, adminPassword, adminHostname+':'+adminPort)
  
 #The path on the local machine where the template will be created, 
 #it should not already exist.
@@ -26,9 +28,40 @@ disconnect()
 #Server. 
 selectCustomTemplate(templatePath)
 loadTemplates()
- 
+
+# set the Node Manager listen address and listen port.
+cd('/')
+cd('NMProperties')
+set('ListenAddress', hostname) 
+
 #create the domain
 writeDomain(domainPath)
 
 # Connect to domain
-connect(adminUsername, adminPassword, adminstHostname+':'+adminPort)
+connect(adminUsername, adminPassword, adminHostname+':'+adminPort)
+
+# Start Edit Session
+edit()
+startEdit()
+
+# Create Machine
+cd('/')
+machine = create(machineName, 'UnixMachine')
+cd('Machines/'+machineName+'/NodeManager/'+machineName)
+cmo.setName(machineName)
+cmo.setListenAddress(hostname)
+
+# Create Managed Server
+cd('/')
+
+# Change Target Host
+cd('/Servers/'+managedServername)
+cmo.setListenAddress(hostname)
+cmo.setMachine(getMBean('/Machines/'+machineName)
+ 
+# Close Session
+save()
+activate(block='true')
+
+#disconnect from online WLST connection to the Administration Server
+disconnect()
---
+++ osb/runNode1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -d -it --name wls1 --add-host osbdb:172.17.0.2 --add-host wlsadmin:172.17.0.3 --hostname wls1 -p 8011:8011 oracle/osb:12.2.1.2 /bin/bash
