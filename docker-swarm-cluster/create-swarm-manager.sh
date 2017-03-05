2ede78f0bf9df11b388c4a11a49edd3c3fc6e3e5
Shuxuan Nie <shuxuan.nie@gmail.com>
2017-03-04 23:36:16 -0800

    update swarm

--- docker-swarm-cluster/create-swarm-manager.sh
+++ docker-swarm-cluster/create-swarm-manager.sh
@@ -1,12 +1,13 @@
-#Swarm initialized: current node (51tjyixnckdw2xjtpv90lic1a) is now a manager.
-#
+docker swarm init --advertise-addr 10.0.2.7
+
+
+#[oracle@swarm-master ~]$ docker swarm init --advertise-addr 10.0.2.7
+#Swarm initialized: current node (5o4dgfqbvdlp7112mnyyi21j4) is now a manager.
+
 #To add a worker to this swarm, run the following command:
-#
+
 #    docker swarm join \
-#    --token SWMTKN-1-0s61uwhyu3sconrk53hnlmyk7s4yu4c5i9zy7zmxns04q9mvrf-cf8pd5h9zdppjf3ikqhrytujo \
-#    10.0.2.10:2377
+#    --token SWMTKN-1-3qkfb6ny69pb278xrin9bb5g4am88lt2iobbr6yx7atduva494-c4meo6o1rn1fdepbdgn7k3ves \
+#    10.0.2.7:2377
 
 #To add a manager to this swarm, run 'docker swarm join-token manager' and follow the instructions.
-
-
-docker swarm init --advertise-addr 10.0.2.10
